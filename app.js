861c1fc7d9825ca9129ceafdad4320b0ae1a5ff9
Kamil Sudol <kamil.sudol5@gmail.com>
2026-01-10 23:50:21 +0100

    2nd javascript task

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log("Kamil");
-console.log("Sudoł");
+let valueInMiles = 1500;
+let valueinKilometers = valueInMiles * 1.60934;
+
+console.log("The distance of " + valueinKilometers + " kms is equal to " + valueInMiles + " miles");
+
+let height = 72;
+let weight = 172;
+
+let cmHeight = 72 * 2.54;
+let kgWeight = 172 / 2.2046;
+
+console.log(cmHeight, kgWeight);
+
+
+let BMI = kgWeight / ((cmHeight / 100) ** 2);
+
+console.log(BMI);
