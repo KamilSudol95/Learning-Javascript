78e8f4411d9f0631701a86246f9bda1d19f86918
Kamil Sudol <kamil.sudol5@gmail.com>
2026-01-11 17:17:18 +0100

    4th javascript task

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const theList = [
-    'Laurence', 'Svekis', true, 35, null, undefined, {test: 'one', score: 55}, ['one', 'two']
-];
+let productCatalogue = [];
 
-theList.pop();
-theList.shift();
-theList.unshift('FIRST');
-theList[3] = 'hello world';
-theList[2] = 'MIDDLE';
-theList[theList.length - 1] = 'LAST';
+let laptop = {
+    name : 'lenovo',
+    model : 'omega',
+    cost : 3799,
+    quantity : 101
+};
 
-console.log(theList)
+let smartphone = {
+    name : 'samsung',
+    model : 's23',
+    cost : 2799,
+    quantity : 50
+};
+
+let gpu = {
+    name : 'nvidia',
+    model : '5080',
+    cost : 4799,
+    quantity : 10
+};
+
+
+productCatalogue.push(laptop, smartphone, gpu);
+console.log(productCatalogue);
+
+let quantity = productCatalogue[2].quantity;
+console.log(quantity);
