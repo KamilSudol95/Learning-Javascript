657fef3f5affb5e6de409071761809a2b9702fe5
Kamil Sudol <kamil.sudol5@gmail.com>
2026-02-14 17:01:14 +0100

    Pizza Menu App conditional rendering using ternary operator

--- learning-react/pizza_menu_app/src/index.jsx
+++ learning-react/pizza_menu_app/src/index.jsx
@@ -37,14 +37,23 @@ function Header() {
 }
 
 function Menu() {
+
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <ul className="pizzas">
-                {pizzaData.map(pizza => (
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
-                ))}
-            </ul>
+            {numPizzas > 0 ? (
+                <ul className="pizzas">
+                    {pizzas.map(pizza => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            ) : (
+                <p>We're still working on our menu. Please come back later:</p>
+            )}
+
         </main>
     )
 }
@@ -57,7 +66,18 @@ function Footer() {
 
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()}.{isOpen ? "We're currently open!" : "Sorry, we're closed"}
+            {isOpen ? (
+                <div className="order">
+                    <p>
+                        We're open until {closeHour}:00. Come visit us or order online
+                    </p>
+                    <button className="btn">Order</button>
+                </div>
+            ): (
+                <p>
+                    We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+                </p>
+            )}
         </footer>
     )
 }
