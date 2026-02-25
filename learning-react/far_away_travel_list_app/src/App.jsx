55e27c4e6a598c2ce2ce3d71f71293f5ad02c8e9
Kamil Sudol <kamil.sudol5@gmail.com>
2026-02-25 23:54:47 +0100

    Far Away Travel List App - reorganised project + applied some finishing touch

--- learning-react/far_away_travel_list_app/src/App.jsx
+++ learning-react/far_away_travel_list_app/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import Logo from './Logo.jsx'
+import Form from './Form.jsx'
+import PackingList from './PackingList.jsx'
+import Stats from './Stats.jsx'
+
 export default function App() {
 
     const [items, setItems] = React.useState([]);
@@ -17,119 +22,25 @@ export default function App() {
         setItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed } : item));
     }
 
+    function handleClearList() {
+        const confirmedDelete = window.confirm('Are you sure you want to delete all items?');
+        if (confirmedDelete) {
+            setItems([]);
+        }
+    }
+
     return (
-        <React.Fragment>
+        <div className="app">
             <Logo />
             <Form onAddItem={handleAddItem} />
-            <PackingList items={items} onDeleteItem={handleDeleteItem}
-            onToggleItem={handleToggleItem}
+            <PackingList
+                items={items}
+                onDeleteItem={handleDeleteItem}
+                onToggleItem={handleToggleItem}
+                onClearList={handleClearList}
             />
-            <Stats items={items}/>
-        </React.Fragment>
-    )
+            <Stats items={items} />
+        </div>
+    );
 }
-
-    function Logo() {
-        return (
-            <h1>🏝️Far Away🧳</h1>
-        )
-    }
-
-    function Form({ onAddItem }) {
-
-        const [description, setDescription] = React.useState('');
-        const [quantity, setQuantity] = React.useState(1);
-
-        function handleSubmit(e) {
-            e.preventDefault();
-
-            if (!description) {
-                return;
-            }
-
-            const newItem = {
-                id: Date.now(),
-                description,
-                quantity,
-                packed: false
-            };
-
-            console.log(newItem);
-
-            onAddItem(newItem);
-
-            setDescription('');
-            setQuantity(1);
-
-        }
-
-        return (
-            <form className="add-form" onSubmit={handleSubmit}>
-                <h3>What do you need for your trip?😎</h3>
-                <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-                    {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-                        <option value={num} key={num}>
-                            {num}
-                        </option>
-                    ))}
-                </select>
-                <input
-                    type='text'
-                    placeholder='Item...'
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                />
-                <button>Add</button>
-            </form>
-        )
-    }
-
-    function PackingList({ items, onDeleteItem, onToggleItem }) {
-        return (
-            <div>
-                <ul className='list'>
-                    {items.map(item => <Item item={item} onDeleteItem={onDeleteItem}
-                                             onToggleItem= {onToggleItem} key={item.id}
-                    />)}
-                </ul>
-            </div>
-        )
-    }
-
-    function Item({ item, onDeleteItem, onToggleItem }) {
-        return (
-            <li>
-                <input type='checkbox' value={item.packed} onChange={() => onToggleItem(item.id)} />
-                <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
-                   {item.quantity} {item.description}
-                </span>
-                <button onClick={() => onDeleteItem(item.id)}>❌</button>
-            </li>
-        )
-    }
-
-    function Stats({ items }) {
-
-        if(!items.length) return (
-            <p className='stats'>
-                <em>
-                    Start adding some items to your packing list 🚀
-                </em>
-            </p>
-        )
-
-        const numItems = items.length;
-        const numPacked = items.filter(item => item.packed).length;
-        const percentage = Math.round(numPacked / numItems * 100);
-
-        return (
-            <footer className="stats">
-                <em >
-                    {percentage === 100 ? 'You got everything! Ready to go 🛫' :
-                    `🧳You have ${numItems} items on your list, and you already packed ${numPacked} items ${percentage}%`
-                    }
-                </em>
-            </footer>
-        )
-    }
 
---
+++ learning-react/far_away_travel_list_app/src/Logo.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Logo() {
+    return (
+        <h1>🏝️Far Away🧳</h1>
+    )
+}
