fd8e06b58be3464c905bc87905c42b0d97666185
Kamil Sudol <kamil.sudol5@gmail.com>
2026-02-25 22:35:25 +0100

    Far Away Travel List App - lifting up state, child to parent communication and practicing derived state

--- learning-react/far_away_travel_list_app/src/App.jsx
+++ learning-react/far_away_travel_list_app/src/App.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 
-
-const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-    { id: 3, description: "Charger", quantity: 1, packed: true },
-]
-
 export default function App() {
+
+    const [items, setItems] = React.useState([]);
+
+
+    function handleAddItem(item) {
+        setItems(items => [...items, item]);
+    }
+
+    function handleDeleteItem(id) {
+        setItems(items => items.filter(item => item.id !== id));
+    }
+
+    function handleToggleItem(id) {
+        setItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed } : item));
+    }
+
     return (
         <React.Fragment>
             <Logo />
-            <Form />
-            <PackingList />
-            <Stats />
+            <Form onAddItem={handleAddItem} />
+            <PackingList items={items} onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
+            />
+            <Stats items={items}/>
         </React.Fragment>
     )
 }
@@ -24,7 +35,7 @@ export default function App() {
         )
     }
 
-    function Form() {
+    function Form({ onAddItem }) {
 
         const [description, setDescription] = React.useState('');
         const [quantity, setQuantity] = React.useState(1);
@@ -44,6 +55,8 @@ export default function App() {
             };
 
             console.log(newItem);
+
+            onAddItem(newItem);
 
             setDescription('');
             setQuantity(1);
@@ -71,31 +84,51 @@ export default function App() {
         )
     }
 
-    function PackingList() {
+    function PackingList({ items, onDeleteItem, onToggleItem }) {
         return (
             <div>
                 <ul className='list'>
-                    {initialItems.map(item => <Item item={item} key={item.id}/>)}
+                    {items.map(item => <Item item={item} onDeleteItem={onDeleteItem}
+                                             onToggleItem= {onToggleItem} key={item.id}
+                    />)}
                 </ul>
             </div>
         )
     }
 
-    function Item({ item }) {
+    function Item({ item, onDeleteItem, onToggleItem }) {
         return (
             <li>
+                <input type='checkbox' value={item.packed} onChange={() => onToggleItem(item.id)} />
                 <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
                    {item.quantity} {item.description}
                 </span>
-                <button>❌</button>
+                <button onClick={() => onDeleteItem(item.id)}>❌</button>
             </li>
         )
     }
 
-    function Stats() {
+    function Stats({ items }) {
+
+        if(!items.length) return (
+            <p className='stats'>
+                <em>
+                    Start adding some items to your packing list 🚀
+                </em>
+            </p>
+        )
+
+        const numItems = items.length;
+        const numPacked = items.filter(item => item.packed).length;
+        const percentage = Math.round(numPacked / numItems * 100);
+
         return (
             <footer className="stats">
-                <em>🧳You have X items on your list, and you already packed X [x%]</em>
+                <em >
+                    {percentage === 100 ? 'You got everything! Ready to go 🛫' :
+                    `🧳You have ${numItems} items on your list, and you already packed ${numPacked} items ${percentage}%`
+                    }
+                </em>
             </footer>
         )
     }
