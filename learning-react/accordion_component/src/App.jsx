e4645593fdf9e311ff3ae47fc68ba3a91c27cc7e
Kamil Sudol <kamil.sudol5@gmail.com>
2026-03-18 20:06:40 +0100

    Accordion component reusable AccordionItem component implemented + final tweaks

--- learning-react/accordion_component/src/App.jsx
+++ learning-react/accordion_component/src/App.jsx
@@ -28,25 +28,53 @@ export default function App() {
 }
 
 function Accordion({data}) {
+
+    const [curOpen, setCurOpen] = React.useState(null);
+
     return <div className='accordion'>
-        {data.map(((el, i ) =><AccordionItem title={el.title} text={el.text} num={i} key={el.title}/>
+        {data.map(((el, i ) =>
+                <AccordionItem
+                    curOpen= {curOpen}
+                    onOpen={setCurOpen}
+                    title={el.title}
+                    num={i}
+                    key={el.title}
+                >
+                    {el.text}
+                </AccordionItem>
         ))}
+
+        <AccordionItem
+            curOpen={curOpen}
+            onOpen={setCurOpen}
+            title="Test 1"
+            num={22}
+            key="test 1"
+        >
+            <p>Allows React developers to:</p>
+            <ul>
+                <li>Break up UI into components</li>
+                <li>Make components reusuable</li>
+                <li>Place state efficiently</li>
+            </ul>
+        </AccordionItem>
+
     </div>;
 }
 
-function AccordionItem({num, title, text}) {
+function AccordionItem({num, title, curOpen, onOpen, children}) {
 
-    const [isOpen, setIsOpen] = React.useState(false);
+    const isOpen = num === curOpen;
 
     function handleToggle() {
-        setIsOpen(!isOpen);
+        onOpen(isOpen ? null : num);
     }
 
     return <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
         <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
         <p className='title'>{title}</p>
         <p className='icon'>{isOpen ? "-" : "+"}</p>
-        {isOpen && <div className='content-box'>{text}</div>}
+        {isOpen && <div className='content-box'>{children}</div>}
     </div>
 
 }
